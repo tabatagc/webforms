47ca33df27664bf338fc962cc430dc9e1e6d9977
Tábata Gonçalves de Carvalho <tabatagc@gmail.com>
2016-09-04 22:43:32 -0300

    Add files via upload

---
+++ TesteImposto/FormImposto.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace TesteImposto
+{
+    partial class FormImposto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNomeCliente = new System.Windows.Forms.TextBox();
+            this.dataGridViewPedidos = new System.Windows.Forms.DataGridView();
+            this.buttonGerarNotaFiscal = new System.Windows.Forms.Button();
+            this.cmbEstadoOrigem = new System.Windows.Forms.ComboBox();
+            this.cmbEstadoDestino = new System.Windows.Forms.ComboBox();
+            this.dataGridNota = new System.Windows.Forms.DataGridView();
+            this.btnCarrega = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridNota)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome do Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Estado Origem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Estado Destino";
+            // 
+            // txtNomeCliente
+            // 
+            this.txtNomeCliente.Location = new System.Drawing.Point(95, 5);
+            this.txtNomeCliente.Name = "txtNomeCliente";
+            this.txtNomeCliente.Size = new System.Drawing.Size(696, 20);
+            this.txtNomeCliente.TabIndex = 3;
+            // 
+            // dataGridViewPedidos
+            // 
+            this.dataGridViewPedidos.AllowUserToOrderColumns = true;
+            this.dataGridViewPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPedidos.Location = new System.Drawing.Point(6, 101);
+            this.dataGridViewPedidos.Name = "dataGridViewPedidos";
+            this.dataGridViewPedidos.Size = new System.Drawing.Size(785, 139);
+            this.dataGridViewPedidos.TabIndex = 7;
+            this.dataGridViewPedidos.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridViewPedidos_DataError);
+            // 
+            // buttonGerarNotaFiscal
+            // 
+            this.buttonGerarNotaFiscal.Location = new System.Drawing.Point(664, 72);
+            this.buttonGerarNotaFiscal.Name = "buttonGerarNotaFiscal";
+            this.buttonGerarNotaFiscal.Size = new System.Drawing.Size(127, 23);
+            this.buttonGerarNotaFiscal.TabIndex = 8;
+            this.buttonGerarNotaFiscal.Text = "Gerar Nota Fiscal";
+            this.buttonGerarNotaFiscal.UseVisualStyleBackColor = true;
+            this.buttonGerarNotaFiscal.Click += new System.EventHandler(this.buttonGerarNotaFiscal_Click);
+            // 
+            // cmbEstadoOrigem
+            // 
+            this.cmbEstadoOrigem.FormattingEnabled = true;
+            this.cmbEstadoOrigem.Items.AddRange(new object[] {
+            "",
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO"});
+            this.cmbEstadoOrigem.Location = new System.Drawing.Point(95, 31);
+            this.cmbEstadoOrigem.Name = "cmbEstadoOrigem";
+            this.cmbEstadoOrigem.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstadoOrigem.TabIndex = 9;
+            // 
+            // cmbEstadoDestino
+            // 
+            this.cmbEstadoDestino.FormattingEnabled = true;
+            this.cmbEstadoDestino.Items.AddRange(new object[] {
+            "",
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "DF",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO"});
+            this.cmbEstadoDestino.Location = new System.Drawing.Point(95, 61);
+            this.cmbEstadoDestino.Name = "cmbEstadoDestino";
+            this.cmbEstadoDestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstadoDestino.TabIndex = 10;
+            // 
+            // dataGridNota
+            // 
+            this.dataGridNota.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridNota.Location = new System.Drawing.Point(12, 290);
+            this.dataGridNota.Name = "dataGridNota";
+            this.dataGridNota.Size = new System.Drawing.Size(779, 150);
+            this.dataGridNota.TabIndex = 11;
+            // 
+            // btnCarrega
+            // 
+            this.btnCarrega.Location = new System.Drawing.Point(668, 258);
+            this.btnCarrega.Name = "btnCarrega";
+            this.btnCarrega.Size = new System.Drawing.Size(122, 23);
+            this.btnCarrega.TabIndex = 12;
+            this.btnCarrega.Text = "Carrega Nota";
+            this.btnCarrega.UseVisualStyleBackColor = true;
+            this.btnCarrega.Click += new System.EventHandler(this.btnCarrega_Click);
+            // 
+            // FormImposto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 477);
+            this.Controls.Add(this.btnCarrega);
+            this.Controls.Add(this.dataGridNota);
+            this.Controls.Add(this.cmbEstadoDestino);
+            this.Controls.Add(this.cmbEstadoOrigem);
+            this.Controls.Add(this.buttonGerarNotaFiscal);
+            this.Controls.Add(this.dataGridViewPedidos);
+            this.Controls.Add(this.txtNomeCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormImposto";
+            this.Text = "Calculo de imposto";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridNota)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNomeCliente;
+        private System.Windows.Forms.DataGridView dataGridViewPedidos;
+        private System.Windows.Forms.Button buttonGerarNotaFiscal;
+        private System.Windows.Forms.ComboBox cmbEstadoOrigem;
+        private System.Windows.Forms.ComboBox cmbEstadoDestino;
+        private System.Windows.Forms.DataGridView dataGridNota;
+        private System.Windows.Forms.Button btnCarrega;
+    }
+}
+
---
+++ TesteImposto/FormImposto.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Imposto.Core.Domain;
+using Imposto.Core.Service;
+using Imposto.Core.Data;
+using System.Data.SqlClient;
+
+namespace TesteImposto
+{
+    public partial class FormImposto : Form
+    {
+        TESTEEntities db = new TESTEEntities();
+
+        public FormImposto()
+        {
+            InitializeComponent();
+            dataGridViewPedidos.AutoGenerateColumns = true;                       
+            dataGridViewPedidos.DataSource = GetTablePedidos();
+            ResizeColumns();
+        }
+
+        private void ResizeColumns()
+        {
+            double mediaWidth = dataGridViewPedidos.Width / dataGridViewPedidos.Columns.GetColumnCount(DataGridViewElementStates.Visible);
+
+            for (int i = dataGridViewPedidos.Columns.Count - 1; i >= 0; i--)
+            {
+                var coluna = dataGridViewPedidos.Columns[i];
+                coluna.Width = Convert.ToInt32(mediaWidth);
+            }   
+        }
+        private object GetTablePedidos()
+        {
+            DataTable table = new DataTable("Pedidos");
+            table.Columns.Add(new DataColumn("Nome", typeof(string)));
+            table.Columns.Add(new DataColumn("Codigo", typeof(string)));
+            table.Columns.Add(new DataColumn("Valor", typeof(decimal)));
+            table.Columns.Add(new DataColumn("Brinde", typeof(bool)));
+
+            return table;
+        }
+        private void buttonGerarNotaFiscal_Click(object sender, EventArgs e)
+        {
+            NotaFiscalService service = new NotaFiscalService();
+            Pedido pedido = new Pedido();
+            DataTable table = (DataTable)dataGridViewPedidos.DataSource;
+
+            pedido.EstadoOrigem = cmbEstadoOrigem.Text;
+            pedido.EstadoDestino = cmbEstadoDestino.Text;
+            pedido.NomeCliente = txtNomeCliente.Text;
+
+            foreach (DataRow row in table.Rows)
+            {
+                PedidoItem item = new PedidoItem();
+                item.NomeProduto = row["Nome"].ToString();
+                item.CodigoProduto = row["Codigo"].ToString();
+                item.ValorItemPedido = Convert.ToDouble(row["Valor"].ToString());
+                if(row["Brinde"].Equals("false")){
+                    item.Brinde = false;
+                }else{
+                    item.Brinde = true;
+                }
+                pedido.ItensDoPedido.Add(item);
+                item = null;
+           }
+
+            if (validar(pedido))
+            {
+                if (service.GerarNotaFiscal(pedido))
+                    MessageBox.Show("Operação efetuada com sucesso");
+                limparCampos();
+            }
+
+
+        }
+        public void limparCampos() {
+            txtNomeCliente.Clear();
+            cmbEstadoOrigem.SelectedIndex = -1;
+            cmbEstadoDestino.SelectedIndex = -1;
+            dataGridViewPedidos.DataSource = null;
+            dataGridViewPedidos.DataSource = GetTablePedidos();
+            ResizeColumns();
+        }
+        private bool validar(Pedido pedido) {
+            string campos = "Por favor preencha os campos: \n";
+            int i = 0;
+
+            if (String.IsNullOrEmpty(pedido.NomeCliente)) {
+                campos = campos + "- Nome \n";
+                i++;
+            }
+
+            if (String.IsNullOrEmpty(pedido.EstadoOrigem))
+            {
+                campos = campos + "- Estado Origem \n";
+                i++;
+            }
+            if (String.IsNullOrEmpty(pedido.EstadoDestino))
+            {
+                campos = campos + "- Estado Destino \n";
+                i++;
+            }
+            if (pedido.ItensDoPedido.Count < 1)
+            {
+                campos = campos + "- Preencha ao menos um item da nota \n";
+                i++;
+            }
+
+            if (i > 0)
+            {
+                MessageBox.Show(campos);
+                return false;
+            }
+            return true;
+        }
+        private void btnCarrega_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Por favor, configurar a String de Conexão.");
+            //Sugestão futura selecionar o id e carregar os dados da nota. 
+            
+            this.dataGridNota.DataSource = db.NotaFiscalItems.ToList();
+        }
+
+        private void dataGridViewPedidos_DataError(object sender, DataGridViewDataErrorEventArgs anError)
+        {
+            MessageBox.Show("Ocorreu um errro: " + anError.Context.ToString());
+
+            if (anError.Context == DataGridViewDataErrorContexts.Commit)
+            {
+                MessageBox.Show("Erro ao inserir. /n");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.CurrentCellChange)
+            {
+                MessageBox.Show("Verifique alteração de campo");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.Parsing)
+            {
+                MessageBox.Show("Verifique o formato do campo /n");
+            }
+            if (anError.Context == DataGridViewDataErrorContexts.LeaveControl)
+            {
+                MessageBox.Show("leave control error");
+            }
+
+            if ((anError.Exception) is ConstraintException)
+            {
+                DataGridView view = (DataGridView)sender;
+                view.Rows[anError.RowIndex].ErrorText = "an error";
+                view.Rows[anError.RowIndex].Cells[anError.ColumnIndex].ErrorText = "an error";
+
+                anError.ThrowException = false;
+            }
+        }
+
+     
+
+
+    }
+}
